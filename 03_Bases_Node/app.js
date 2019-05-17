faa9f88f8f01769582becd582b67687796d26732
ralvarellos-evoltis <ralvarellos@evoltis.com>
2019-05-17 18:01:55 -0300

    Updates + Console App

---
+++ 03_Bases_Node/app.js
@@ -0,0 +1,46 @@
+// Requires
+const argv = require('yargs')
+    .command('listar', 'Imprime en consola la tabla de multiplicar', {
+        base: {
+            demand: true,
+            alias: 'b'
+        },
+        limite: {
+            alias: 'l',
+            default: 10
+        }
+    })
+    .command('crear', 'Crear tabla de multiplicar', {
+        base: {
+            demand: true,
+            alias: 'b'
+        },
+        limite: {
+            alias: 'l',
+            default: 10
+        }
+    })
+    .help()
+    .argv;
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
+
+let comando = argv._[0];
+
+switch (comando) {
+    case 'listar':
+        console.log('Listar');
+        listarTabla(argv.base, argv.limite);
+
+        break;
+    case 'crear':
+        console.log('Crear');
+        crearArchivo(argv.base, argv.limite)
+            .then(archivo => console.log(`Archivo creado: ${ archivo }`))
+            .catch(e => console.log(e));
+
+        break;
+
+    default:
+        console.log('Comando no reconocido');
+        break;
+}
---
+++ 03_Bases_Node/multiplicar/multiplicar.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+
+
+
+let listarTabla = (base, limite = 10) => {
+
+    for (let i = 1; i <= limite; i++) {
+
+        console.log(`${ base } * ${i} = ${ base * i }\n`);
+
+    }
+
+};
+
+
+crearArchivo = (base, limite = 10) => {
+
+    return new Promise((resolve, reject) => {
+
+        if (!Number(base)) {
+            reject(`El valor introducido como base ${ base } no es un número`);
+            return;
+        }
+
+        let data = '';
+
+        for (let i = 1; i <= limite; i++) {
+
+            data += (`${ base } * ${i} = ${ base * i }\n`);
+
+        }
+
+
+        fs.writeFile(`tablas/tabla-del-${ base }-al-${ limite }.txt`, data, (err) => {
+
+            if (err)
+                reject(err);
+            else
+                resolve(`tabla-del-${ base }-al-${ limite }.txt`);
+        });
+
+    });
+
+};
+
+module.exports = {
+    crearArchivo,
+    listarTabla
+};
